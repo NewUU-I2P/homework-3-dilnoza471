74072b985d97609711be975518290cea47e95b30
ibadlayevam19 <m.ibadlayeva@newuu.uz>
2023-11-10 18:06:00 +0500

    update

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,9 @@
-void problemSolution1(float consumed_water) {
-    float cost;
-    // write your code here
-
+float problemSolution1(float consumed_water) {
+    float cost = 0;
+    if (consumed_water <= 30) cost = 0.4*consumed_water;
+    else if (consumed_water<=50) cost = 12+(consumed_water-30)*0.12;
+    else if (consumed_water<=60) cost = 14.4 + (consumed_water-50)*1.4;
+    else cost = 28.4+(consumed_water-60)*1.5;
+    cost += 13;
     return cost;
 }
